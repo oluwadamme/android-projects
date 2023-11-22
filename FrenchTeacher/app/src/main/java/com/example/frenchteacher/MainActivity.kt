534510015e21784f9aa1394b01bb3d7b2eb0d934
Damilola Adeniyi <adeniiyidamilola246@gmail.com>
2023-11-22 21:30:39 +0100

    french teaching app

---
+++ FrenchTeacher/app/src/main/java/com/example/frenchteacher/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.frenchteacher
+
+import android.annotation.SuppressLint
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+
+
+    fun  sayTheColor(view: View){
+        val clickedButton:Button = view as Button
+
+        val mediaPlayer:MediaPlayer = MediaPlayer.create(this,
+            resources.getIdentifier(
+                clickedButton.tag.toString(),"raw",packageName
+            )
+         )
+        mediaPlayer.start()
+
+    }
+}
